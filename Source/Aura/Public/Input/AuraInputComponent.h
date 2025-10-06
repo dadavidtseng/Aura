169eef4881bf87a85b710bfd9ead19456c141a21
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-10-06 09:22:54 -0500

    feat(input): add AuraInputComponent base class
    Create custom enhanced input component extending UEnhancedInputComponent.
    This establishes the foundation for ability-based input binding system.
    
    - Add AuraInputComponent class with standard component lifecycle
    - Prepare for template-based ability action binding
    - Part of enhanced input integration for GAS

---
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -0,0 +1,26 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "AuraInputComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AURA_API UAuraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAuraInputComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
